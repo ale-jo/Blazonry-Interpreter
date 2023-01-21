359eff1699f1e4a1de32aa6d485190c80c3bd5c1
Aleksandra Jordanović <ana.jordanovic@hotmail.com>
2023-01-21 16:17:31 +0100

    Grammar updated(Lines of Partition) and generated models(dot files)

--- blazonry/__init__.py
+++ blazonry/__init__.py
@@ -12,7 +12,7 @@ def blazonry_language():
     "blazonry language"
     current_dir = os.path.dirname(__file__)
     mm = metamodel_from_file(os.path.join(current_dir, 'blazonry.tx'))
-
+    my_model = mm.model_from_file('p.txt')
     # Here if necessary register object processors or scope providers
     # http://textx.github.io/textX/stable/metamodel/#object-processors
     # http://textx.github.io/textX/stable/scoping/
@@ -25,4 +25,8 @@ def blazonry_generate_files(metamodel, model, output_path, overwrite, debug):
     # input_file = model._tx_filename
     # output_dir = output_path if output_path else dirname(input_file)
     # print(output_dir, ' output dir')
-    print("success")
+    print("    = = = == = = success")
+    current_dir = os.path.dirname(__file__)
+    mm = metamodel_from_file(os.path.join(current_dir, 'blazonry.tx'), debug= True)
+    my_model = mm.model_from_file('p.txt')
+    # print(my_model.fileds[0].field.__str__)
